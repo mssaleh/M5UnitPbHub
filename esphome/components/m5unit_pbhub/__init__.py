88267e77fa6aeb31a2c1f3d590aa8d23b43a979d
Mohammed Saleh <mssaleh@live.com>
2024-05-29 10:08:53 +0400

    fix

--- esphome/components/m5unit_pbhub/__init__.py
+++ esphome/components/m5unit_pbhub/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from esphome import automation
+from esphome.const import CONF_ID, CONF_ADDRESS, CONF_CHANNEL
 
 DEPENDENCIES = ['i2c']
 
@@ -10,11 +10,11 @@ M5UnitPbHub = m5unit_pbhub_ns.class_('M5UnitPbHub', cg.Component, i2c.I2CDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(M5UnitPbHub),
-    cv.Optional('address', default=0x38): cv.i2c_address,
-    cv.Optional('channel', default=0): cv.int_,
-}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema())
+    cv.Optional(CONF_ADDRESS, default=0x61): cv.i2c_address,
+    cv.Optional(CONF_CHANNEL, default=0): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x61))
 
 def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()], config['address'], config['channel'])
+    var = cg.new_Pvariable(config[CONF_ID], config[CONF_ADDRESS], config[CONF_CHANNEL])
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
